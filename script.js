68268f820774228eff6004f29cf232566d5e9fb1
twelvepointfour <christopherrobinsullivan@gmail.com>
2025-10-21 08:43:55 +1030

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const quotes = [
+    {
+        quote: "When does a joke become a dad joke?",
+        answer: "When it becomes apparent!"
+    },
+    {
+        quote: "How many apples grow on a tree?",
+        answer: "All of them!"
+    },
+    {
+        quote: "What do you call a fish with no eyes?",
+        answer: "Fsh."
+    },
+    {
+        quote: "Why don't scientists trust atoms?",
+        answer: "Because they make up everything!"
+    }
+];
+
+const newQuoteBtn = document.getElementById('new-quote-btn');
+const quoteText = document.getElementById('quote-text'); 
+const quoteAnswer = document.getElementById('quote-answer'); 
+
+function displayRandomQuote () {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+
+    quoteText.textContent = randomQuote.quote;
+    quoteAnswer.textContent = randomQuote.answer;
+}
+
+newQuoteBtn.addEventListener('click', displayRandomQuote);
+
+displayRandomQuote();
