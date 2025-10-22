249c094605f7e4aafa49c5cae3f98e1a25298a53
twelvepointfour <christopherrobinsullivan@gmail.com>
2025-10-22 22:16:51 +1030

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const quotes = [
         answer: "But I've started taking steps to avoid it."
     },
     {
-        quote: "Two peanuts were walking down the street."
+        quote: "Two peanuts were walking down the street.",
         answer: "One was a-salted!"
     },
     {
@@ -29,8 +29,53 @@ const quotes = [
     },
     {
         quote: "How do you tell the difference between a frog and a horny toad?",
-        answer: "A frog says "ribbit, ribbit", and a horny toad says "rub it, rub it""
+        answer: "A frog says 'ribbit, ribbit', and a horny toad says 'rub it, rub it'"
+    },
+    {
+        quote: "What do you call a man with no body and no nose?",
+        answer: "Nobody nose!"
+    },
+    {
+        quote: "What's the difference between a poorly dressed man on a tricycle, and a well-dressed man on a bicycle?",
+        answer: "Attire."
+    },
+    {
+        quote: "Dad, I'm hungry",
+        answer: "Hi hungry, I'm dad"
+    },
+    {
+        quote: "What sits on the lawn and is Irish?",
+        answer: "Patty O'furniture"
+    },
+    {
+        quote: "Did you hear about the guy who invented lifesavers?",
+        answer: "They say he made a mint!"
+    },
+    {
+        quote: "Dracula doesn't have many friends.",
+        answer: "Because he's a pain in the neck."
+    },
+    {
+        quote: "I don't fear condiments on my food,",
+        answer: "I relish them!"
+    },
+    {
+        quote: "Velcro...",
+        answer: "What a rip-off!"
+    },
+    {
+        quote: "There's an new type of broom.",
+        answer: "It's sweeping the nation."
+    },
+    {
+        quote: "conjunctivitis.com",
+        answer: "Now there's a site for sore eyes!"
+    },
+    {
+        quote: "Never trust an acupuncturist.”, 
+        answer: “They always stab you in the back!”
     }
+
 ];
 
 const newJokeBtn = document.getElementById('new-joke-btn');
